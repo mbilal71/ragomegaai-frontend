9cb17f4527c4ee21985d73fab7c8f45f3576a9a0
Mounas82 <abdulrehman03082007@gmail.com>
2024-09-26 22:15:05 +0500

    Nextjs project added

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Page = () => {
+  return (
+    <div>
+      Main page where user can chat with the chatbot. Both options will be available here. You can use a button for uploading document and there will be a dropdown menu which will have three options 1-LLM 2-RAG 3-Both. You can customize the design as you wish.
+    </div>
+  )
+}
+
+export default Page
